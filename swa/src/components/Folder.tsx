ae0cf9cf3c6b87f4ca0fe4901b2a3b0ea8724a20
Peter Paul Kirschner <peter@kirschner.zone>
2024-06-23 21:51:53 +0200

    ux change

--- swa/src/components/Folder.tsx
+++ swa/src/components/Folder.tsx
@@ -28,7 +28,7 @@ export function Folder(props: IFolderEXProps) {
     };
     return (
         <div>
-            <div>
+            <div className="flex justify-between">
                 <h3 className=" text-2xl font-bold inline">
                     {isRenaming ? <div>
                         <input  title="rename" type="text" 
